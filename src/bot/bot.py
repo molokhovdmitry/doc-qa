099ed2abd8cec714dd97bc6454227df7291613e6
Александр Горбунов <santerr80@mail.ru>
2024-05-21 18:27:23 +0700

    Урал токен из скрипта

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,8 +1,13 @@
 import telebot
 from telebot import types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Токен телеграм бота
-TOKEN = '6724916194:AAGTp_bZ_MN-kGkvQu9lOf71HCoF_hLElEY'
+TOKEN = os.getenv("TOKEN")
 
 # Создаем экземпляр бота
 bot = telebot.TeleBot(TOKEN)
@@ -11,11 +16,12 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, я телеграм-бот ПЭК, чем могу помочь?")
+        bot.send_message(message.from_user.id, "Привет, я телеграм-бот ПЭК,чем могу помочь?")
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Напиши любой запрос к базе данных документов ПЭК.")
-    else:
-        bot.send_message(message.from_user.id, message.text) # добавить функцию обработки запроса к langchain
+    else:                   # добавить функцию обработки запроса к langchain
+        bot.send_message(message.from_user.id, message.text)
+
 
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
