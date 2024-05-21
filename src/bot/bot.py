56448385df2be2148171b81e8fd39b691ea7365d
Александр Горбунов <santerr80@mail.ru>
2024-05-21 16:32:58 +0700

    Update

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -7,14 +7,15 @@ TOKEN = '6724916194:AAGTp_bZ_MN-kGkvQu9lOf71HCoF_hLElEY'
 # Создаем экземпляр бота
 bot = telebot.TeleBot(TOKEN)
 
+# Текстовый обработчик
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, я телеграм-бот ПЭК, чем могу помочь?")
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши любой запрос к базе данных документов")
+        bot.send_message(message.from_user.id, "Напиши любой запрос к базе данных документов ПЭК.")
     else:
         bot.send_message(message.from_user.id, message.text)
 
-
+# Запускаем бота
 bot.polling(none_stop=True, interval=0)
